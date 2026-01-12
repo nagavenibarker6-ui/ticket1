4c6d8ae78ba0b31765445e21db5f358fbbb7504b
Nagaveni.Barker <nagavenibarker6@gmail.com>
2026-01-12 09:51:42 +0530

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -4,11 +4,21 @@ class MovieBooking:
         self.movie_name = movie_name
         self.ticket_price = ticket_price
         self.ticket_count = ticket_count
-
     def total_bill(self):
         return self.ticket_price * self.ticket_count
-
     def booking_status(self):
         if self.ticket_count <= 0:
             return "Invalid Booking"
         return "Booking Confirmed"
+def main():
+    booking = MovieBooking(1, "Toxic", 250, 4)
+    print("===== MOVIE BOOKING DETAILS =====")
+    print(f"Booking ID   : {booking.booking_id}")
+    print(f"Movie Name   : {booking.movie_name}")
+    print(f"Ticket Price : {booking.ticket_price}")
+    print(f"Ticket Count : {booking.ticket_count}")
+    print(f"Total Bill   : {booking.total_bill()}")
+    print(f"Status       : {booking.booking_status()}")
+    print("================================")
+if __name__ == "__main__":
+    main()
